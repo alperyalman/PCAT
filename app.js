7c58de5c168192d1529c3107022473448d2fc904
Alper Yalman <alperyalman@gmail.com>
2022-08-17 11:28:47 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
+/* const myLogger = (req, res, next) => {
+  console.log('Middleware log 1');
+  next();
+};
+
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware log 2');
+  next();
+}; */
+
+//MIDDLEWARE
+app.use(express.static('public'));
+/* app.use(myLogger);
+app.use(myLogger2); */
+
 app.get('/', (req, res) => {
-  
-  const photo = {
-    id: 1,
-    name: "Photo name",
-    description: "Photo description"
-  }
-  res.send(photo);
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 const port = 3000;
